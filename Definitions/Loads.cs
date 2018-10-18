fbf091178ec7237df5c85abc78807b8454348cc5
Suhail Sheth <suhail.yusuf@econstruct.ae>
2018-10-18 10:22:28 +0400

    1.0.1.3

--- Definitions/Loads.cs
+++ Definitions/Loads.cs
@@ -8,6 +8,32 @@ namespace DynamoETABS.Definitions
 {
     public class Loads
     {
-       
+        //Load Case Name List
+        internal List<string> LoadCase { get; set; }
+        //Loading Value List }
+        internal List<double> LoadValue { get; set; }
+        //Loading Type
+        internal String LoadType { get; set; }
+        
+        //Creates Load Object
+        public static Loads CreateLoads(List<string> Loadcase, List<double> Loadvalue, String Loadtype = "Line")
+        {
+
+            Loads Load = new Loads(Loadcase,Loadvalue,Loadtype);
+
+            return Load;
+
+        }
+
+        //Load Object Constructor
+        internal Loads() { }
+        internal Loads(List<string> loadcase, List<double> loadvalue, string loadtype)
+        {
+            LoadCase = loadcase;
+            LoadValue = loadvalue;
+            LoadType = loadtype;
+        }
+
+
     }
 }
